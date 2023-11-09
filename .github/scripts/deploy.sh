def0a3b593af33fdd5125278eb7f1384ab300d2e
Nick <nick@gmx.co.uk>
2023-11-09 17:53:57 +0200

    Create deployment script + create yaml for deployment + change workflow

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+DEPLOY_ID=$(aws deploy create-deployment --cli-input-yaml file://create-deployment.yaml --region eu-central-1)
+
+while true; do
+  STATUS=$(aws deploy get-deployment --deployment-id $DEPLOY_ID --query 'deploymentInfo.status' --output text)
+  if [ $STATUS != "InProgress" ] && [ $STATUS != "Created" ]; then
+    if [ $STATUS = "Succeeded" ]; then
+       echo "SUCCESS"
+       exit 0
+    else
+       echo "Failed"
+       exit 1
+    fi
+  else
+    echo "Deploying..."
+  fi
+     sleep 30
+done
