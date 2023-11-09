4efa2dbafcbf6aa0c4dc034c585348af6272a0dd
Nick <nick@gmx.co.uk>
2023-11-09 18:13:01 +0200

    Fix deploy id output

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 
-DEPLOY_ID=$(aws deploy create-deployment --cli-input-yaml file://create-deployment.yaml --region eu-central-1)
+DEPLOY_ID=$(aws deploy create-deployment --cli-input-yaml file://create-deployment.yaml --region eu-central-1 --output text)
 
 while true; do
   STATUS=$(aws deploy get-deployment --deployment-id $DEPLOY_ID --query 'deploymentInfo.status' --output text)
+           aws deploy get-deployment --deployment-id d-1TSIPDJ8F --query 'deploymentInfo.status' --output text
   if [ $STATUS != "InProgress" ] && [ $STATUS != "Created" ]; then
     if [ $STATUS = "Succeeded" ]; then
        echo "SUCCESS"
