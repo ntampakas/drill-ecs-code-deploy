4fc3e5bc28c35bffc2fafd0b705901364de97333
Nick <nick@gmx.co.uk>
2023-11-10 14:50:12 +0200

    Minnor changes due to new deployment group

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -x
+set -ex
 
 DEPLOY_ID=$(aws deploy create-deployment --cli-input-yaml file://create-deployment.yaml --region eu-central-1 --output text)
 
 while true; do
   STATUS=$(aws deploy get-deployment --deployment-id $DEPLOY_ID --query 'deploymentInfo.status' --output text)
-           aws deploy get-deployment --deployment-id d-1TSIPDJ8F --query 'deploymentInfo.status' --output text
   if [ $STATUS != "InProgress" ] && [ $STATUS != "Created" ]; then
     if [ $STATUS = "Succeeded" ]; then
        echo "SUCCESS"
